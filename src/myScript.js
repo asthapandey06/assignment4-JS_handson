11265d726de5a0722463776e4b3a32e0318c5757
Astha <asthapandey2022jan@cedcoss.com>
2022-02-05 14:12:41 +0530

    golf code

--- src/myScript.js
+++ src/myScript.js
@@ -1,24 +1,26 @@
-function testSize(num) {
-    // Only change code below this line
-    if(num < 5) {
-       return "Tiny";}
-    else if(num < 10){
-      return "Small";
-    } 
-    else if(num < 15){
-      return "Medium";
-    }
-    else if(num < 20){
-      return "Large";
-    } 
-    else if(num >= 20){
-      return "Huge";
-    } 
-  
-  
-  
-    return "Change Me";
-    // Only change code above this line
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+     return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+     return "Eagle";
+  } else if (strokes == par - 1) {
+     return "Birdie";
+  } else if (strokes == par) {
+     return "Par";
+  } else if (strokes == par + 1) {
+     return "Bogey";
+  } else if (strokes == par + 2) {
+     return "Double Bogey";
+  }else if (strokes >= par + 3) {
+     return "Go Home!";
   }
-  
-  testSize(7);
+  // Only change code above this line
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
